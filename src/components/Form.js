2af049907041ebc849b78e343b4af390827079d6
Miriam Völkl <miriamvoelkl@gmail.com>
2024-01-14 20:35:43 +0100

    set up form component and add event handler

---
+++ src/components/Form.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+export default function Form(onAddActivity) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formElements = event.target.elements;
+    const data = {
+      name: formElements.inputActivity.value,
+      isForGoodWeather: formElements.isForGoodWeather.checked,
+    };
+    console.log(data);
+    // onAddActivity(data);
+    event.target.reset();
+    formElements.inputActivity.focus();
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <h1>✨Add a new activity✨</h1>
+        <label htmlFor="input-activity">Name of the activity</label>
+        <input name="inputActivity" id="input-activity" type="text"></input>
+        <label htmlFor="input-weather">
+          🌞Is it an activity for good weather?🌞
+        </label>
+        <input
+          name="isForGoodWeather"
+          id="input-weather"
+          type="checkbox"
+        ></input>
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
+}
